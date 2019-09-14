7db1bf5da69d873775aca606fe43948575696de2
tbirrell <tbirrell@xristos.net>
2019-09-14 18:17:51 -0500

    allow recipes to be viewed without login

--- routes/web.php
+++ routes/web.php
@@ -14,26 +14,29 @@
 
 Auth::routes();
 
+
 //=== AUTH WALL ===//
-Route::middleware(['auth'])->group(function(){
-	Route::get('/', 'RecipeController@index');
-
-
-/* 
- |----------------------------------------------------------------|
- | Actions Handled By Resource Controller 												|
- |----------------------------------------------------------------|
- | VERB 			|	URI 									| ACTION 	| ROUTE NAME 			|
- | -----------|-----------------------|---------|-----------------|
- | GET				| /photos								|	index		|	photos.index		|
- | GET				| /photos/create				|	create	|	photos.create		|
- | POST				| /photos								|	store		|	photos.store		|
- | GET				| /photos/{photo}				|	show		|	photos.show			|
- | GET				| /photos/{photo}/edit	|	edit		|	photos.edit			|
- | PUT/PATCH	| /photos/{photo}				|	update	|	photos.update		|
- | DELETE			| /photos/{photo}				|	destroy	|	photos.destroy	|
- |----------------------------------------------------------------|
- */
- 
-  Route::resource('recipes', 'RecipeController');
+Route::middleware(['auth'])->group(function () {
+    Route::get('/', 'RecipeController@index');
+    
+    
+    /*
+     |-----------------------------------------------------------------|
+     | Actions Handled By Resource Controller                          |
+     |-----------------------------------------------------------------|
+     | VERB       |  URI                  | ACTION   |  ROUTE NAME     |
+     | -----------|-----------------------|----------|-----------------|
+     | GET        | /photos               |  index   |  photos.index   |
+     | GET        | /photos/create        |  create  |  photos.create  |
+     | POST       | /photos               |  store   |  photos.store   |
+     | GET        | /photos/{photo}       |  show    |  photos.show    |
+     | GET        | /photos/{photo}/edit  |  edit    |  photos.edit    |
+     | PUT/PATCH  | /photos/{photo}       |  update  |  photos.update  |
+     | DELETE     | /photos/{photo}       |  destroy |  photos.destroy |
+     |-----------------------------------------------------------------|
+     */
+    
+    Route::resource('recipes', 'RecipeController')->except(['show']);
 });
+
+Route::get('recipes/{recipe}', 'RecipeController@show')->name('recipes.show');
